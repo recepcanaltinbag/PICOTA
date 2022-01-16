bb2d95145334b9762c795f89c99bfb987ea7b048
Recep Can Altınbağ <recepcanaltinbag@gmail.com>
2022-01-16 13:44:25 +0300

    Update quality_control_module.py

--- quality_control_module.py
+++ quality_control_module.py
@@ -19,26 +19,26 @@ def raw_read_filtering(raw_file, file_end_type, q_type, qual_threshold, out_fold
     if file_end_type == "se":
         raw_file_1_path = raw_file[0]
         filtered_raw_file_1_name = "filtered_" + raw_file_1_path.split('/')[-1].split('.')[0] 
-        f1_path = "out_folder/" + filtered_raw_file_1_name +".fastq"
+        f1_path = out_folder + "/" + filtered_raw_file_1_name +".fastq"
         args = f"programs/sickle/sickle se -f {raw_file_1_path} -t {q_type} -q {qual_threshold} -o {f1_path}"
         #my_process = subprocess.run(args, shell=True, executable='/bin/bash', text=True, check=True)
-        filtering_visualization(raw_file_1_path)
-        filtering_visualization(f1_path)
+        filtering_visualization(raw_file_1_path, out_folder)
+        filtering_visualization(f1_path, out_folder)
 
     elif file_end_type == "pe":
         raw_file_1_path = raw_file[0]
         raw_file_2_path = raw_file[1]
         filtered_raw_file_1_name = "filtered_" + raw_file_1_path.split('/')[-1].split('.')[0]
         filtered_raw_file_2_name = "filtered_" + raw_file_2_path.split('/')[-1].split('.')[0]
-        f1_path = "out_folder/" + filtered_raw_file_1_name +".fastq"
-        f2_path = "out_folder/" + filtered_raw_file_2_name +".fastq"
+        f1_path = out_folder + "/" + filtered_raw_file_1_name +".fastq"
+        f2_path = out_folder + "/" + filtered_raw_file_2_name +".fastq"
         args = f"programs/sickle/sickle pe --pe-file1 {raw_file_1_path} --pe-file2 {raw_file_2_path} \
             --qual-type {q_type} -q {qual_threshold} --output-pe1 {f1_path} --output-pe2 {f2_path}" 
         my_process = subprocess.run(args, shell=True, executable='/bin/bash', text=True, check=True)
-        filtering_visualization(raw_file_1_path)
-        filtering_visualization(f1_path)
-        filtering_visualization(raw_file_2_path)
-        filtering_visualization(f2_path)
+        filtering_visualization(raw_file_1_path, out_folder)
+        filtering_visualization(f1_path, out_folder)
+        filtering_visualization(raw_file_2_path, out_folder)
+        filtering_visualization(f2_path, out_folder)
  
 
 # ----------------------------------------------------------------
@@ -51,6 +51,6 @@ fastqc seqfile1 seqfile2 .. seqfileN
            [-c contaminant file] seqfile1 .. seqfileN
 
 '''
-def filtering_visualization(file_path):
-    args = f"programs/fastqc/fastqc {file_path} -o raw_input" 
+def filtering_visualization(file_path, out_folder):
+    args = f"programs/fastqc/fastqc {file_path} -o {out_folder}" 
     my_process = subprocess.run(args, shell=True, executable='/bin/bash', text=True, check=True)
