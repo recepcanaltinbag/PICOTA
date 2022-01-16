3ac59dbccd966ab005c7d87ae228e0c8bf108ed4
Recep Can Altınbağ <recepcanaltinbag@gmail.com>
2022-01-16 14:05:20 +0300

    Create annotation_module.py

---
+++ annotation_module.py
@@ -0,0 +1,31 @@
+import subprocess
+import os
+import glob
+from Bio import SeqIO
+from isescan_parser import parse_driver
+from isescan_parse import parse_novel_driver
+
+# ----------------------------------------------------------------
+# 8- ANNOTATION
+# ----------------------------------------------------------------
+'''
+# Choose the names of the output files
+% prokka --outdir mydir --prefix mygenome contigs.fa
+'''
+def annotation_driver(out_filtered_path, annotation_out):
+    if not os.path.exists(annotation_out):
+        os.makedirs(annotation_out)
+    
+    args = f"prokka --outdir {annotation_out} {out_filtered_path}" 
+    my_process = subprocess.run(args, shell=True, executable='/bin/bash', text=True, check=True)
+   
+  
+  
+def annotation_driver_for_novelty(IS_result_file, cycle_file, out_novel_path, annotation_out):
+    parse_novel_driver(IS_result_file, cycle_file, out_novel_path)
+    
+    if not os.path.exists(annotation_out):
+        os.makedirs(annotation_out)
+    
+    args = f"prokka --outdir {annotation_out} {out_novel_path}" 
+    my_process = subprocess.run(args, shell=True, executable='/bin/bash', text=True, check=True)
