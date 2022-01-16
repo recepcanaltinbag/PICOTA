1bd39e0c2e74bb5b161fc4b7f6b7266ffa75c7bd
Recep Can Altınbağ <recepcanaltinbag@gmail.com>
2022-01-16 14:02:57 +0300

    Update isescan_parser.py

--- isescan_parser.py
+++ isescan_parser.py
@@ -36,5 +36,25 @@ def parse_driver(IS_result_file, cycle_file, out_filtered_path):
         out_filtered_file.write(filtered_fasta)
 
 
+def parse_novel_driver(IS_result_file, cycle_file, out_novel_path):
+    file_read = open(IS_result_file,'r')
+    file_lines = parse_IS_result_file(file_read)
 
+    if file_lines != False:
+        seq_list = []
+        for line in file_lines:
+            seq_id = line.split(' ')[0]
+            if seq_id not in seq_list:
+                seq_list.append(seq_id)
+
+        filtered_fasta = ""
+        with open(cycle_file) as handle:
+            for record in SeqIO.parse(handle, "fasta"):
+                
+                if record.id not in seq_list:
+                    filtered_fasta = filtered_fasta + '>' + str(record.id) + '\n' + str(record.seq) + '\n'
+
+
+        out_filtered_file = open(out_filtered_path,"w")
+        out_filtered_file.write(filtered_fasta)
 
